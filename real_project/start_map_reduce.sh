909b7086773cd9f9425ef839654ac89d519167ed
Tim Lukas Nolte <tim.lukas.nolte@stud.uni-hannover.de>
2024-01-24 14:28:45 +0100

    Added script for compiling, changed start map reduce script to use compiled versions

--- real_project/start_map_reduce.sh
+++ real_project/start_map_reduce.sh
@@ -12,13 +12,13 @@ trap 'stop_processes' EXIT
 AMOUNT_WORKERS=$1
 # Start your programs in the background
 cd ./grpc/master/
-go run . $1 & 
+./master $1 & 
 echo starting master with ${AMOUNT_WORKERS} workers
 cd ../worker/
 for (( i=1; i<=$AMOUNT_WORKERS; i++ ))
 do
     echo starting worker $i at port 5006"$i"
-    go run . $i 50051 5006"$i" &
+    ./worker $i 50051 5006"$i" &
 done
 
 # Keep the script running
